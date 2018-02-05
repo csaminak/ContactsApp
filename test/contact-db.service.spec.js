ef746de5670d728fd245be0c54f504e9dede1bbe
Samina Chhatwal <csaminak@gmail.com>
2018-02-05 01:29:15 -0500

    setting up the tests for the service file which will take care of all contact data handling

---
+++ test/contact-db.service.spec.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+
+  var assert = chai.assert;
+
+  suite('ContactDbService Tests', function() {
+
+    setup(module('contactsApp'));
+
+    test('', function() {
+
+
+    });
+
+
+  });
+
+
+}());
