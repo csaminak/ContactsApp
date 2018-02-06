fad5412ce7247193336e71d99821c49f69e12a72
Samina Chhatwal <csaminak@gmail.com>
2018-02-05 20:55:37 -0500

    injecting paramaters and assigning them to the variables so that they can be used through out the test suite

--- test/contact-db.service.spec.js
+++ test/contact-db.service.spec.js
@@ -4,8 +4,15 @@
   var assert = chai.assert;
 
   suite('ContactDbService Tests', function() {
+    var ContactDbService, $rootScope;
 
     setup(module('contactsApp'));
+
+    setup(inject(function(ContactDbService, _$rootScope_) {
+      ContactDbService = contactDbService;
+      $rootScope = _$rootScope_;
+
+    }));
 
     test('', function() {
 
